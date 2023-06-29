cd449d43eb26a426af33863a8eef210e3ea459ad
stepan <stephen.jr@selyuk.com>
2023-06-29 14:52:04 +0300

    Typescriptization of Project

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,18 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './components/App/app';
-import createStore from './services/index';
-// test commit signed
-
-const store = createStore()
+import App from './components/app/app';
+import store from './services/index';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-
-    <Provider store={store}>
+    <Provider store={store()}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-
 );
 
--- src/pages/spinner/spinner.js
+++
@@ -1,9 +0,0 @@
-import spinnerStyles from './spinner.module.css';
-
-const Spinner = () => {
-    return (
-        <div className={spinnerStyles.spinner} />
-    );
-}
-
-export default Spinner;
